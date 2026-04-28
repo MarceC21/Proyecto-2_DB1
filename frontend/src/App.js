0c4b961601a5a0092f3c3e8d79796793b3a89444
MarceC21 <marcelap.castillo2102@gmail.com>
2026-04-28 00:08:45 -0600

    Implementación de la navegación

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,26 @@
-import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Productos from "./pages/Productos";
+import Clientes from "./pages/Clientes";
+import Ventas from "./pages/Ventas";
+import Compras from "./pages/Compras";
 
-function App() {
-  const [mensaje, setMensaje] = useState("");
 
-  useEffect(() => {
-    fetch("http://localhost:5000/test")
-      .then(res => res.json())
-      .then(data => setMensaje(data.message))
-      .catch(err => console.error(err));
-  }, []);
-
+// Esta página solo va a mostrar el navbar 
+export default function App() {
   return (
-    <div>
-      <h1>Hola desde React</h1>
-      <h2>{mensaje}</h2>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Navigate to="/ventas" replace />} />
+          <Route path="/ventas" element={<Ventas />} />
+          <Route path="/productos" element={<Productos />} />
+          <Route path="/clientes" element={<Clientes />} />
+          <Route path="/compras" element={<Compras />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
-
-export default App;
